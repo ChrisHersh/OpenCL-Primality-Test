6ca19b936cef9055a1d136129e6afb9c6b25dc80
Christopher Hersh <ch5968@cs.ship.edu>
2015-07-19 18:23:33 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+This is a test for opencl using Haskell
+
+to run first do cabal install c2hs then
+
+cabal install opencl --extra-include-dirs="c:\Program Files (x86)\AMD APP SDK\3.0-0-Beta\bin\x86_64" --extra-lib-dirs="c:\Program Files (x86)\AMD APP SDK\3.0-0-Beta\lib\x86_64"
+ 
+Note: You will need the OpenCL Library for your GPU chipset manufacturer and change the paths accordingly
---
+++ gpuPrimalityTest.c
@@ -0,0 +1,81 @@
+
+
+int power(int a, int n, int mod)
+{
+	int power = a;
+	int result = 1;
+
+	while (n)
+	{
+		if (n & 1)
+			result = (result * power) % mod;
+		power = (power * power) % mod;
+		n >>= 1;
+	}
+	return result;
+}
+
+bool witness(int n, int s, int d, int a)
+{
+	int x = power(a, d, n);
+	int y;
+
+	while (s) {
+		y = (x * x) % n;
+		if (y == 1 && x != 1 && x != n - 1)
+			return 0;
+		x = y;
+		--s;
+	}
+	if (y != 1)
+		return 0;
+	return 1;
+}
+
+int is_prime_mr(int n)
+{
+	if (((!(n & 1)) && n != 2) || (n < 2) || (n % 3 == 0 && n != 3))
+		return 0;
+	if (n <= 3)
+		return 1;
+
+	int d = n / 2;
+	int s = 1;
+	while (!(d & 1)) {
+		d /= 2;
+		++s;
+	}
+
+	if (n < 1373653)
+		return witness(n, s, d, 2) && witness(n, s, d, 3);
+	if (n < 9080191)
+		return witness(n, s, d, 31) && witness(n, s, d, 73);
+	if (n < 4759123141)
+		return witness(n, s, d, 2) && witness(n, s, d, 7) && witness(n, s, d, 61);
+	if (n < 1122004669633)
+		return witness(n, s, d, 2) && witness(n, s, d, 13) && witness(n, s, d, 23) && witness(n, s, d, 1662803);
+	if (n < 2152302898747)
+		return witness(n, s, d, 2) && witness(n, s, d, 3) && witness(n, s, d, 5) && witness(n, s, d, 7) && witness(n, s, d, 11);
+	if (n < 3474749660383)
+		return witness(n, s, d, 2) && witness(n, s, d, 3) && witness(n, s, d, 5) && witness(n, s, d, 7) && witness(n, s, d, 11) && witness(n, s, d, 13);
+	return witness(n, s, d, 2) && witness(n, s, d, 3) && witness(n, s, d, 5) && witness(n, s, d, 7) && witness(n, s, d, 11) && witness(n, s, d, 13) && witness(n, s, d, 17);
+}
+
+__kernel void duparray(__global int *in, __global int *out )
+{
+	int id = get_global_id(0);
+
+	// if(id == 1)
+	// {
+	// 	int i = 0;
+	// 	for(i = 0; i <= 200; i++)
+	// 	{
+	// 		printf("%d\t", in[i]);
+	// 	}
+	// }
+
+	//printf("%d :: %d :: %d\t\t",id, in[id], is_prime_mr(in[id]));
+	out[id] = (is_prime_mr(in[id]) == 0) ? 0 : in[id];
+	printf("%d :: %d :: %d\t\t",id, in[id], is_prime_mr(in[id]));
+	//out[id] = in[id];
+}
